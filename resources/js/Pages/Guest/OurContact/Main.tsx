eb88ad3cc019bd88d1c5a6e1bb2c6c939fd92c66
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2025-03-19 05:42:34 +0800

    fix(our-contact): fixing responsive design

--- resources/js/Pages/Guest/OurContact/Main.tsx
+++ resources/js/Pages/Guest/OurContact/Main.tsx
@@ -10,19 +10,33 @@ import { ReactNode } from "react";
 
 export default function Page(): ReactNode
 {
+    const hostName = window.location.hostname == 'smp-blbs-smr.sch.id' ? 'smp-blbs-smr.sch.id' : 'sma-blbs-smr.sch.id'
+
+    const emailHostName = [
+        {
+            host:'sma-blbs-smr.sch.id',
+            email:'smaplusbudiluhur@gmail.com'
+        },
+        {
+            host:'smp-blbs-smr.sch.id',
+            email:'smpplusbudiluhur@gmail.com'
+        }
+    ]
+
+    const findByHostName = emailHostName.find(({host}) => host == hostName)
     return (
         <>
             <Head title="Kontak Kami" />
             <GuestLayout>
                 <section id="about-us" className="relative isolate py-40 px-5 pb-5 lg:pb-2 md:pb-3 lg:px-10 min-h-screen sm:py-40 font-outfit">
-                    <div className="flex flex-col justify-items-center items-center px-10 w-full">
+                    <div className="flex flex-col justify-items-center items-center w-full">
                         <div className="w-full lg:w-2/4 md:w-2/4 sm:w-3/4 mb-5">
                             <p className="text-xl font-outfit text-center">
                                 Kontak Kami
                             </p>
                             <hr className="mt-2 border-1 border-sky-500 mb-5" />
                         </div>
-                        <Card className="w-2/4">
+                        <Card className="w-full lg:w-2/4 md:w-3/4 sm:w-3/4">
                             <CardHeader>
                                 <h3 className="text-2xl font-bold text-center">Detail Kontak</h3>
                             </CardHeader>
@@ -35,13 +49,13 @@ export default function Page(): ReactNode
                                     <div className="flex items-center space-x-2">
                                         <Avatar className="w-4 h-4" />
                                         <a href="mailto:smaplusbudiluhur@gmail.com" target="_blank">
-                                            Email : smaplusbudiluhur@gmail.com
+                                            Email : {findByHostName!.email}
                                         </a>
                                     </div>
                                 </div>
                             </CardContent>
                         </Card>
-                        <Card className="w-2/4 mt-3 mb-3">
+                        <Card className="w-full lg:w-2/4 md:w-3/4 sm:w-3/4 mt-3 mb-3">
                             <CardHeader>
                                 <h3 className="text-2xl font-bold">Leave a Message</h3>
                             </CardHeader>
